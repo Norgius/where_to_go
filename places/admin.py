2ce9fc76977998235f1f28ce53129569b95c1eb8
Norgius <dmitry.schadrin21@yandex.ru>
2022-12-06 22:12:59 +0300

    Made easy to add photos in admin

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    raw_id_fields = ('place', )
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
-
+    inlines = [ImageInline, ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    class Meta:
+        model = Image
