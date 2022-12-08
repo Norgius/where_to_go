38c2d58a9fd69c4dcbf15a156a5f1bb40dc5e611
Norgius <101505413+Norgius@users.noreply.github.com>
2022-12-08 23:38:39 +0300

    Add choose static_root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,7 +125,12 @@ STATIC_URL = env.str('STATIC_URL', '/static/')
 
 MEDIA_URL = env.str('MEDIA_URL', '/media/')
 
-STATIC_ROOT = env.str('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = env.str('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 MEDIA_ROOT = env.str('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
 
