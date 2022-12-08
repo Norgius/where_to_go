f31cd12265cf1d744c76a12858b8a9b059681dd4
Norgius <101505413+Norgius@users.noreply.github.com>
2022-12-08 23:36:19 +0300

    DEBUG=False

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', '.pythonanywhere.com'])
 
