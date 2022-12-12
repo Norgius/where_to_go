ee8fd4efa97c17d3e1cd7293c4c12588f23b0366
Norgius <dmitry.schadrin21@yandex.ru>
2022-12-12 22:35:41 +0300

    Now SECRET_KEY must be passed to .env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -20,12 +20,11 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
