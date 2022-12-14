558f312e87eca2c266d08cd706d36e5f42e6e5d7
Norgius <dmitry.schadrin21@yandex.ru>
2022-12-14 20:23:19 +0300

    Edit Model Fields

--- places/models.py
+++ places/models.py
@@ -4,10 +4,10 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=150, unique=True)
-    description_short = models.TextField('Короткое описание')
-    description_long = HTMLField('Длинное описание')
-    lng = models.FloatField('Долгота', null=True)
-    lat = models.FloatField('Широта', null=True)
+    description_short = models.TextField('Короткое описание', blank=True)
+    description_long = HTMLField('Длинное описание', blank=True)
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
 
     def __str__(self):
         return self.title
@@ -20,11 +20,7 @@ class Image(models.Model):
         related_name='images',
         on_delete=models.SET_NULL,
         null=True)
-    img = models.ImageField(
-        'Картинка',
-        upload_to='',
-        null=True,
-        blank=True)
+    img = models.ImageField('Картинка')
     imagetitle = models.CharField(
         'Название изображения',
         max_length=70,
